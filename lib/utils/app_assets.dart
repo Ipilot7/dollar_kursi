827dc3ab91f3761848a4a073505f402b3b46e832
Muhammadqodir <muwicef@gmail.com>
2023-05-18 18:57:32 +0500

    add utils file

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,56 @@
+abstract class AppAssets {
+  AppAssets._();
+
+  static AppIcons get icons => AppIcons();
+
+  static AppImages get images => AppImages();
+}
+
+abstract class AssetsFolder {
+  final String basePath;
+
+  AssetsFolder(this.basePath);
+}
+
+class AppIcons extends AssetsFolder {
+  AppIcons() : super('assets/icons');
+
+  String get search => '$basePath/ic-search.svg';
+  String get drawer => '$basePath/ic-drawer.svg';
+  String get back => '$basePath/ic-back.svg';
+  String get more => '$basePath/ic-more.svg';
+  // Top Bar Icons
+
+  String get notifications => '$basePath/ic-notifications.svg';
+  String get appereance => '$basePath/ic-appearance.svg';
+  String get privacy => '$basePath/ic-privacy.svg';
+  String get about => '$basePath/ic-about.svg';
+  // Settings Section Icons
+
+  String get settingsFilled => '$basePath/ic-settings-filled.svg';
+  String get homeFilled => '$basePath/ic-home-filled.svg';
+  String get settings => '$basePath/ic-settings.svg';
+  String get home => '$basePath/ic-home.svg';
+  // Navigation Bar Icons
+
+  String get filter => '$basePath/ic-filter.svg';
+  String get dollar => '$basePath/ic-dollar.svg';
+  String get down => '$basePath/ic-dollar.svg';
+  String get up => '$basePath/ic-dollar.svg';
+  // Home Screen Icons
+
+  String get circle => '$basePath/ic-dollar.svg';
+  // Drawer Icons
+}
+
+class AppImages extends AssetsFolder {
+  AppImages() : super('assets/images');
+
+  String get bank1 => '$basePath/bank-1.png';
+  String get bank2 => '$basePath/bank-2.png';
+  String get bank3 => '$basePath/bank-3.png';
+  String get bank4 => '$basePath/bank-4.png';
+  String get bank5 => '$basePath/bank-5.png';
+  String get bank6 => '$basePath/bank-6.png';
+  // Bank Photos
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static Color onPrimaryContainer = const Color(0xFF001C38);
+  static Color primaryContainer = const Color(0xFFD3E4FF);
+  static Color secondarySurface = const Color(0xFFEFF1F8);
+  static Color primarySurface = const Color(0xFFF3F4F9);
+  static Color onSurface20 = const Color(0xFF001E2F);
+  static Color onSurface40 = const Color(0xFF44474E);
+  static Color onSurface60 = const Color(0xFF74777F);
+  static Color background = const Color(0xFFFCFCFF);
+  static Color primary = const Color(0xFF1B72C0);
+  static Color text = const Color(0xFF1B1C1F);
+}
