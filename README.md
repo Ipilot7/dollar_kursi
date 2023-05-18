ea7446317c94221f47447b11f8e57d2d1de3f2c1
Muhammadqodir <muwicef@gmail.com>
2023-05-18 15:28:18 +0500

    Create empty project

---
+++ README.md
@@ -0,0 +1,3 @@
+# dollar_kursi
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dollar_kursi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dollar_kursi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
