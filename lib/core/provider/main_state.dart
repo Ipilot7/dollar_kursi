4211ca12fe3c1c73d8ead75fdd728a9ab94bd825
Ipilot7 <93022197+Ipilot7@users.noreply.github.com>
2025-08-03 15:32:21 +0400

    version is update

--- lib/core/provider/main_state.dart
+++ lib/core/provider/main_state.dart
@@ -5,66 +5,6 @@ import 'package:hive/hive.dart';
 import '../services/bank_service.dart';
 import '../models/bank_model.dart';
 
-// class MainAppState extends ChangeNotifier {
-//   int selectedIndex = 0;
-//   int sortIndex = 0;
-
-//   final GlobalKey<ScaffoldState> key = GlobalKey();
-//   List<BankModel> allBanks = [];
-//   var box = Hive.box('banks');
-
-//   MainAppState() {
-//     getBanks();
-//   }
-
-//   void changePage(int index) {
-//     selectedIndex = index;
-//     notifyListeners();
-//   }
-
-//   void getBanks() async {
-//     allBanks = await BankService.getBanks();
-//     box.isEmpty ? addBox(allBanks) : null;
-//     notifyListeners();
-//   }
-
-//   void addBox(List<BankModel> elements) {
-//     for (var element in elements) {
-//       box.add(
-//         BankModel(
-//           bank: element.bank,
-//           buy: element.buy,
-//           sell: element.sell,
-//         ),
-//       );
-//     }
-//   }
-
-//   void changeSortIndex(int index) {
-//     sortIndex = index;
-//     sortDatas();
-//     notifyListeners();
-//   }
-
-//   void sortDatas() {
-//     List<BankModel> data = box.values.toList().cast();
-
-//     switch (sortIndex) {
-//       case 0:
-//         data.sort((a, b) => a.bank!.name!.compareTo(b.bank!.name!));
-//         break;
-//       case 1:
-//         data.sort((a, b) => b.buy!.compareTo(a.buy!));
-//         break;
-//       case 2:
-//         data.sort((a, b) => a.sell!.compareTo(b.sell!));
-//         break;
-//       default:
-//         data.sort((a, b) => a.bank!.name!.compareTo(b.bank!.name!));
-//     }
-//   }
-// }
-
 class MainAppState extends ChangeNotifier {
   int selectedIndex = 0;
   int sortIndex = 0;
@@ -87,17 +27,16 @@ class MainAppState extends ChangeNotifier {
 
   void getBanks() async {
     allBanks = await BankService.getBanks();
+    await box.clear();
     if (box.isEmpty) addBox(allBanks);
     notifyListeners();
   }
 
   void addBox(List<BankModel> elements) {
     for (var element in elements) {
-      box.add(BankModel(
-        bank: element.bank,
-        buy: element.buy,
-        sell: element.sell,
-      ));
+      box.add(
+        BankModel(bank: element.bank, buy: element.buy, sell: element.sell),
+      );
     }
   }
 
@@ -108,7 +47,21 @@ class MainAppState extends ChangeNotifier {
   }
 
   void sortDatas() {
-    // your sorting logic
+    List<BankModel> data = box.values.toList().cast();
+
+    switch (sortIndex) {
+      case 0:
+        data.sort((a, b) => a.bank!.name!.compareTo(b.bank!.name!));
+        break;
+      case 1:
+        data.sort((a, b) => b.buy!.compareTo(a.buy!));
+        break;
+      case 2:
+        data.sort((a, b) => a.sell!.compareTo(b.sell!));
+        break;
+      default:
+        data.sort((a, b) => a.bank!.name!.compareTo(b.bank!.name!));
+    }
   }
 
   void setSearchQuery(String query) {
